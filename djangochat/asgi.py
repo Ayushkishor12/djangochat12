5846996022822ee70892d9e23baa6818bb2acf60
ayush <ayushkishor307@gmail.com>
2025-01-20 22:32:29 +0530

    tst

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -5,7 +5,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-from room.routing import websocket_urlpatterns  # Adjust if your routing is in a different app
+from room.routing import websocket_urlpatterns  # Adjust if your routing is in a different appp
 
 # Set the default settings module for the 'django' program.
 
